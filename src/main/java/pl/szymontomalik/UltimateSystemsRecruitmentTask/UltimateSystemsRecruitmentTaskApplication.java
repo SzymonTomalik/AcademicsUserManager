11a9363f09152802084f5cf15d15dda96a8fc232
SzymonTomalik <stomalik@gmail.com>
2021-04-03 22:47:50 +0200

    Initial commit

---
+++ src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/UltimateSystemsRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.szymontomalik.UltimateSystemsRecruitmentTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateSystemsRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateSystemsRecruitmentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/UltimateSystemsRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymontomalik.UltimateSystemsRecruitmentTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateSystemsRecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

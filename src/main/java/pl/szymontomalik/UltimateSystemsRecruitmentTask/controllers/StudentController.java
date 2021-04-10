5a5bbb9d7bec0a617382c6513a30cc46b9a5fac1
SzymonTomalik <stomalik@gmail.com>
2021-04-10 23:14:49 +0200

    crud functionality

--- src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/controllers/StudentController.java
+++ src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/controllers/StudentController.java
@@ -4,23 +4,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.szymontomalik.UltimateSystemsRecruitmentTask.entities.Student;
+import pl.szymontomalik.UltimateSystemsRecruitmentTask.services.StudentService;
 import pl.szymontomalik.UltimateSystemsRecruitmentTask.services.ViewService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/students")
 public class StudentController {
     private final ViewService service;
+    private final StudentService studentService;
 
     @RequestMapping("")
     public String viewStudentList(Model model) {
         return viewPageOfStudentList(model, 1, "id", "asc", null, true);
     }
 
-    @GetMapping("/page/{pageNum}")
+    @RequestMapping("/page/{pageNum}")
     public String viewPageOfStudentList(Model model, @PathVariable(name = "pageNum") int pageNum,
                                         @Param("sortField") String sortField,
                                         @Param("sortDir") String sortDir,
@@ -28,4 +32,43 @@ public class StudentController {
                                         @Param("flag") boolean flag) {
         return service.selectUserListForStudentPage(model, pageNum, sortField, sortDir, keyword, flag);
     }
+
+    @GetMapping("/add")
+    public String addNewStudentForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "studentForm";
+    }
+
+    @PostMapping("/add")
+    public String saveNewStudent(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            studentService.save(student);
+            return "redirect:/students";
+        }
+        return "studentForm";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editStudent(Model model, @PathVariable("id") Long studentId) {
+        Student student = studentService.get(studentId);
+        model.addAttribute("student", student);
+        return "editStudentForm";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String saveEditedStudent(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editStudentForm";
+        }
+        studentService.save(student);
+        return "redirect:/students";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable("id") Long studentId) {
+        studentService.delete(studentId);
+        return "redirect:/students";
+    }
+
 }
--- src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/controllers/TeacherController.java
+++ src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/controllers/TeacherController.java
@@ -4,16 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.szymontomalik.UltimateSystemsRecruitmentTask.entities.Teacher;
+import pl.szymontomalik.UltimateSystemsRecruitmentTask.services.TeacherService;
 import pl.szymontomalik.UltimateSystemsRecruitmentTask.services.ViewService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/teachers")
 public class TeacherController {
     private final ViewService service;
+    private final TeacherService teacherService;
 
     @RequestMapping("")
     public String viewTeacherList(Model model) {
@@ -21,7 +25,7 @@ public class TeacherController {
     }
 
 
-    @GetMapping("/page/{pageNum}")
+    @RequestMapping("/page/{pageNum}")
     public String viewPageOfTeacherList(Model model, @PathVariable(name = "pageNum") int pageNum,
                                         @Param("sortField") String sortField,
                                         @Param("sortDir") String sortDir,
@@ -29,4 +33,43 @@ public class TeacherController {
                                         @Param("flag") boolean flag) {
         return service.selectUserListForTeacherPage(model, pageNum, sortField, sortDir, keyword, flag);
     }
+
+    @GetMapping("/add")
+    public String addNewTeacherForm(Model model) {
+        Teacher teacher = new Teacher();
+        model.addAttribute("teacher", teacher);
+        return "teacherForm";
+    }
+
+    @PostMapping("/add")
+    public String saveNewTeacher(@Valid @ModelAttribute("teacher") Teacher teacher, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "teacherForm";
+        }
+        teacherService.save(teacher);
+        return "redirect:/teachers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editTeacher(Model model, @PathVariable("id") Long teacherId) {
+        Teacher teacher = teacherService.get(teacherId);
+        model.addAttribute("teacher", teacher);
+        return "editTeacherForm";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String saveEditedTeacher(@Valid @ModelAttribute("teacher") Teacher teacher, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editTeacherForm";
+        }
+        teacherService.save(teacher);
+        return "redirect:/teachers";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTeacher(@PathVariable("id") Long teacherId) {
+        teacherService.delete(teacherId);
+        return "redirect:/teachers";
+    }
+
 }

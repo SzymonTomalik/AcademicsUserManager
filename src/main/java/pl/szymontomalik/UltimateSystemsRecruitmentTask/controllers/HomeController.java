944766497672420314f8bc9d83f28a0ff3fe1a2e
SzymonTomalik <stomalik@gmail.com>
2021-04-11 22:46:49 +0200

    page view improvement

---
+++ src/main/java/pl/szymontomalik/UltimateSystemsRecruitmentTask/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package pl.szymontomalik.UltimateSystemsRecruitmentTask.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+}
